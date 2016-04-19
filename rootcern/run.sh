35d04116b2f6b1332b52d8b45a7f58f0d385fd1d
MBoretto <marco.boretto@cern.ch>
2016-04-19 08:45:40 +0200

    introducing root cern

--- rootcern/run.sh
+++ rootcern/run.sh
@@ -1,29 +1,7 @@
 #!/bin/bash
 
-#DOCKUSERNAME='developer'
-#docker run -ti --rm \
-#    -v `pwd`/${DOCKUSERNAME}:/home/${DOCKUSERNAME} \
-#	-v /tmp/.X11-unix:/tmp/.X11-unix \
-#	-e DISPLAY=unix$DISPLAY \
-#	na62-farm
-
-
 docker run -it --rm \
    -v /tmp/.X11-unix:/tmp/.X11-unix \
    -e DISPLAY=unix$DISPLAY \
 	root-cern
-
-#docker run -it --rm \
-#   --security-opt "apparmor:docker-ptrace" \
-#   -v `pwd`/root:/root \
-#   -v /tmp/.X11-unix:/tmp/.X11-unix \
-#   -e DISPLAY=unix$DISPLAY \
-#	na62-farm
-
-#docker run -it --rm \
-#   -v `pwd`/root:/root \
-#   -v /tmp/.X11-unix:/tmp/.X11-unix \
-#   -e DISPLAY=unix$DISPLAY \
-#   --security-opt "apparmor:docker-ptrace" \
-#	na62-farm
 
