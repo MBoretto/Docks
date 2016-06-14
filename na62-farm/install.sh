e98caa7c9c0e39b62837c6dfb2ae8b0ee11a0344
MBoretto <marco.boretto@cern.ch>
2016-06-14 15:39:47 +0200

    improving install

--- na62-farm/install.sh
+++ na62-farm/install.sh
@@ -16,7 +16,6 @@ else
     echo 'Fetching remote Na62 Repository...'
     git clone https://github.com/NA62/na62-farm-lib.git
     git clone https://github.com/NA62/na62-trigger-algorithms.git
-    git clone https://github.com/NA62/na62-trigger-test.gi
     git clone https://github.com/NA62/na62-farm-lib-networking.git
     git clone https://github.com/NA62/na62-farm-dim-interface.git
     git clone https://github.com/NA62/na62-farm.git
@@ -24,7 +23,7 @@ else
     git clone https://github.com/NA62/na62-farm-telsim.git
     git clone https://github.com/NA62/na62-farm-monitor-collector.git
     #trigger test
-    git clone https://github.com/NA62/na62-trigger-test.git
+    #git clone https://github.com/NA62/na62-trigger-test.git
     #Script
     git clone https://github.com/NA62/na62-farm-misc.git
     cd ${BASEDIR}
