7265c9ed2975bb42073b95de5b1f6d42ccb3cbed
MBoretto <marco.bore@gmail.com>
2016-06-30 16:42:33 +0200

    pfring compiled in the shared folder

--- na62-farm/compilePFRING.sh
+++ na62-farm/compilePFRING.sh
@@ -3,8 +3,12 @@
 wget 'downloads.sourceforge.net/project/ntop/PF_RING/PF_RING-6.4.1.tar.gz?r=&ts=1466602362&use_mirror=ufpr' -O PF_RING-6.4.1.tar.gz
 
 echo "==> extracting"
-tar -xzvf PF_RING-6.4.1.tar.gz
-cd PF_RING-6.4.1
+
+PFRING_PATH='/root'
+
+mkdir $PFRING_PATH
+tar -xzvf PF_RING-6.4.1.tar.gz -C $PFRING_PATH
+cd $PFRING_PATH/PF_RING-6.4.1
 make
 
 cd kernel; make install
